a8d31276f66c4ec48eee7a8f6053d123bb919ab8
tsonobe <sono.tak.st@gmail.com>
2022-12-24 16:09:09 +0900

    Initial Commit

---
+++ dailyCalender/dailyCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyCalenderApp.swift
+//  dailyCalender
+//
+//  Created by 薗部拓人 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct dailyCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
